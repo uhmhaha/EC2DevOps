31627a1ca84251698ab57980a101576ecc32e96c
jumingyu <knighp@gmail.com>
2020-06-20 19:46:45 +0900

    19th commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@
 # sudo chmod 755 /var/www/server.js # optional
 # crontab -l | { cat; echo "@reboot pm2 start /var/www/index.js -i 0 --name \"node-app\""; } | crontab -
 # sudo pm2 start /var/www/index.js -i 0 --name "node-app"
-npm install
+sudo npm install
 pm2 stop /home/ubuntu/FirstNodeApp/index.js
 pm2 start /home/ubuntu/FirstNodeApp/index.js -i 0
