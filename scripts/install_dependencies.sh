b19f167cc24d8ab2cc146aa7756035bc352dda20
jumingyu <knighp@gmail.com>
2020-06-20 20:54:58 +0900

    27th commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/FirstNodeApp
-sudo npm install
-sudo apt update -y
+npm install
+apt update -y
 
 # get node into yum
 # curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
