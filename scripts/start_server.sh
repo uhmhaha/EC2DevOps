d8c06a6ce0b2d428163487fec0ec261a9cb57aab
jumingyu <knighp@gmail.com>
2020-06-20 16:23:08 +0900

    second commit

---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# sudo chmod 755 /var/www/server.js # optional
+# crontab -l | { cat; echo "@reboot pm2 start /var/www/index.js -i 0 --name \"node-app\""; } | crontab -
+
+sudo pm2 stop node-app
+
+sudo pm2 start /var/www/index.js -i 0 --name "node-app"
