06ad75e99066e5730cbe22cf108fa470218664e1
jumingyu <knighp@gmail.com>
2020-06-20 17:13:22 +0900

    8th commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,4 @@
 # sudo chmod 755 /var/www/server.js # optional
 # crontab -l | { cat; echo "@reboot pm2 start /var/www/index.js -i 0 --name \"node-app\""; } | crontab -
 
-
 sudo pm2 start /var/www/index.js -i 0 --name "node-app"
